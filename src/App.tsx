9aa5decc046f63ebd31de9eb49064c1c6fca7ecf
chan9yu <dev.cgyeo@gmail.com>
2025-02-16 22:00:09 +0900

    chore: tailwind css v4 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-	return <div>Hello World!</div>;
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+			<h1 className="text-3xl font-bold">Hello world!</h1>
+		</div>
+	);
 }
